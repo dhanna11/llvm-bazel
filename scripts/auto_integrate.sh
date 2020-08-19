79ee5e8de6b4da9f06011d83503b270634440e4d
Geoffrey Martin-Noble <gcmn@google.com>
2020-08-18 23:51:27 -0700

    Create action for automatically integrating LLVM

---
+++ scripts/auto_integrate.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# This file is licensed under the Apache License v2.0 with LLVM Exceptions.
+# See https://llvm.org/LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+./scripts/traverse_llvm_revs.sh ./scripts/generate_build_and_tag.sh
